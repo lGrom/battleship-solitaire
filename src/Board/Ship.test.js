fbfa32c06431c0bc8c2f2b4571e5469cd2ba4af3
Grom <person.japescapes@gmail.com>
2024-01-22 17:54:29 -0600

    added cycling state via clicking

--- src/Board/Ship.test.js
+++ src/Board/Ship.test.js
@@ -1,49 +1,49 @@
 import Ship, { GRAPHICAL_TYPES, INTERNAL_TYPES, PLAY_TYPES } from "./Ship"
 
-test('change types', () => {
+test('set types', () => {
     // play types
-    expect(new Ship(PLAY_TYPES.UKNOWN).changePlayType(PLAY_TYPES.WATER).playType).toBe(PLAY_TYPES.WATER);
+    expect(new Ship(PLAY_TYPES.UKNOWN).setPlayType(PLAY_TYPES.WATER).playType).toBe(PLAY_TYPES.WATER);
     expect(() => {
-        new Ship(PLAY_TYPES.UKNOWN).changePlayType(GRAPHICAL_TYPES.UP);
+        new Ship(PLAY_TYPES.UKNOWN).setPlayType(GRAPHICAL_TYPES.UP);
     }).toThrow('Invalid input');
 
     // graphical types
-    expect(new Ship(PLAY_TYPES.UKNOWN).changeGraphicalType(GRAPHICAL_TYPES.LEFT).graphicalType).toBe(GRAPHICAL_TYPES.LEFT);
+    expect(new Ship(PLAY_TYPES.UKNOWN).setGraphicalType(GRAPHICAL_TYPES.LEFT).graphicalType).toBe(GRAPHICAL_TYPES.LEFT);
     expect(() => {
-        new Ship(PLAY_TYPES.UKNOWN).changeGraphicalType(INTERNAL_TYPES.VERTICAL);
+        new Ship(PLAY_TYPES.UKNOWN).setGraphicalType(INTERNAL_TYPES.VERTICAL);
     }).toThrow('Invalid input');
 
     // internal types
-    expect(new Ship(PLAY_TYPES.UKNOWN).changeInternalType(INTERNAL_TYPES.HORIZONTAL).internalType).toBe(INTERNAL_TYPES.HORIZONTAL);
+    expect(new Ship(PLAY_TYPES.UKNOWN).setInternalType(INTERNAL_TYPES.HORIZONTAL).internalType).toBe(INTERNAL_TYPES.HORIZONTAL);
     expect(() => {
-        new Ship(PLAY_TYPES.UKNOWN).changeInternalType(10);
+        new Ship(PLAY_TYPES.UKNOWN).setInternalType(10);
     }).toThrow('Invalid input');
 })
 
-test('change types propogation', () => {
+test('set types propogation', () => {
     const ship = new Ship(PLAY_TYPES.UKNOWN);
 
-    ship.changePlayType(PLAY_TYPES.WATER);
+    ship.setPlayType(PLAY_TYPES.WATER);
     expect(ship.graphicalType).toBe(GRAPHICAL_TYPES.WATER);
     expect(ship.internalType).toBe(INTERNAL_TYPES.WATER);
 
-    ship.changePlayType(PLAY_TYPES.SHIP);
+    ship.setPlayType(PLAY_TYPES.SHIP);
     expect(ship.graphicalType).toBe(GRAPHICAL_TYPES.SHIP);
     expect(ship.internalType).toBe(INTERNAL_TYPES.SHIP);
 
-    ship.changeGraphicalType(GRAPHICAL_TYPES.LEFT);
+    ship.setGraphicalType(GRAPHICAL_TYPES.LEFT);
     expect(ship.playType).toBe(PLAY_TYPES.SHIP);
     expect(ship.internalType).toBe(INTERNAL_TYPES.LEFT);
 
-    ship.changeGraphicalType(PLAY_TYPES.WATER);
+    ship.setGraphicalType(PLAY_TYPES.WATER);
     expect(ship.playType).toBe(PLAY_TYPES.WATER);
     expect(ship.internalType).toBe(INTERNAL_TYPES.WATER);
 
-    ship.changeInternalType(INTERNAL_TYPES.VERTICAL);
+    ship.setInternalType(INTERNAL_TYPES.VERTICAL);
     expect(ship.playType).toBe(PLAY_TYPES.SHIP);
     expect(ship.graphicalType).toBe(GRAPHICAL_TYPES.SHIP);
 
-    ship.changeInternalType(INTERNAL_TYPES.UKNOWN);
+    ship.setInternalType(INTERNAL_TYPES.UKNOWN);
     expect(ship.playType).toBe(PLAY_TYPES.UKNOWN);
     expect(ship.graphicalType).toBe(GRAPHICAL_TYPES.UKNOWN);
 })
