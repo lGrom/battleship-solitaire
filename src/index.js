be5c9364714f36e86e068d18bfba232ea545a274
Grom <93106270+lGrom@users.noreply.github.com>
2024-09-23 19:29:56 -0500

    updated eslint to the latest version (9.11.0) and updated the action to use the new config file.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
